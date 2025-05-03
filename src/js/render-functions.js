159f31731d53d3c06ab65031f2ce0d5b5746c981
BilalFirtina <b.firtana@gmail.com>
2025-05-03 23:19:36 +0300

    test

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -12,24 +12,36 @@ const lightbox = new SimpleLightbox('.gallery a', {
 
 export const createGallery = function (images,position) {
     const createdHtml = images.map(({ largeImageURL, webformatURL, tags, likes, views, comments, downloads }) => {
-        return `<div class="img-div"><a href="${largeImageURL}"><img class="image" src="${webformatURL}" alt="${tags
+        return `<li class="image-li">
+           <a href="${largeImageURL}"> <img class="li-img" src="${webformatURL}" alt="${tags
           .split(',')
           .slice(0, 3)
-          .join(',')}"></a>
-    <section class="inner-section">
-      <div class="info-div">
-        <strong>Likes</strong>${likes}
-      </div>
-      <div class="info-div">
-        <strong>Views</strong>${views}
-      </div>
-      <div class="info-div">
-        <strong>Comments</strong>${comments}
-      </div>
-      <div class="info-div">
-        <strong>Downloads</strong>${downloads}
-      </div>
-    </section></div>`;
+          .join(',')}"> </a>
+          <div class="div-upper">
+            <ul>
+            <li>
+              <div class="div-inner"><b>Likes</b>
+            ${likes}</div>
+            </li>
+
+            <li>
+             <div class="div-inner"><b>Views</b>
+            ${views}</div>
+            </li>
+
+            <li>
+              <div class="div-inner"><b>Comments</b>
+            ${comments}</div>
+            </li>
+
+            <li>
+             <div class="div-inner"><b>Downloads</b>
+            ${downloads}</div>
+            </li>
+
+            </ul>
+          </div>
+        </li>`;
     }).join("");
     gallery.insertAdjacentHTML(position, createdHtml);
   lightbox.refresh();
